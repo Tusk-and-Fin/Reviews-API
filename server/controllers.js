7e25716aadb6fc8d7d20dc29c62f6ddd7f0be9f7
Patrick Alexandre <palexandre822@gmail.com>
2023-09-06 19:57:45 -0700

    deployed DB

--- server/controllers.js
+++ server/controllers.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: process.env.PORT,
 });
 
